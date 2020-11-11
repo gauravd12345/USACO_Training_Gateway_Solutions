9aac524afd0c9e6fb9bbbace68423d2a67cc4a5a
gauravd12345 <55636921+gauravd12345@users.noreply.github.com>
2020-11-11 11:10:08 -0800

    Update beads.cpp

--- Chapter 1/Section 1.2/beads.cpp
+++ Chapter 1/Section 1.2/beads.cpp
@@ -1,1 +1,22 @@
-a
+/*
+ID: icystuf1
+PROG: beads
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+using namespace std;
+
+
+
+int main()
+{
+    ifstream fin("beads.in");
+    ofstream fout("beads.out");
+
+    
+}
