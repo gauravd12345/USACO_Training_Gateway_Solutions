23d4ef7a20b28c4e14dcc28512e850212f4dec64
gauravd12345 <55636921+gauravd12345@users.noreply.github.com>
2020-11-11 11:10:45 -0800

    Update friday.cpp

--- Chapter 1/Section 1.2/friday.cpp
+++ Chapter 1/Section 1.2/friday.cpp
@@ -1,1 +1,22 @@
-a
+/*
+ID: icystuf1
+PROG: friday
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+using namespace std;
+
+
+
+int main()
+{
+    ifstream fin("friday.in");
+    ofstream fout("friday.out");
+
+    
+}
