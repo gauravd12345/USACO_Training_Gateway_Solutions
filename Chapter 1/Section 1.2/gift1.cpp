a8ea832c89398db519efd4e2e9e06f091d2eb8c5
gauravd12345 <55636921+gauravd12345@users.noreply.github.com>
2020-11-11 11:11:49 -0800

    Update gift1.cpp

--- Chapter 1/Section 1.2/gift1.cpp
+++ Chapter 1/Section 1.2/gift1.cpp
@@ -1,1 +1,22 @@
-a
+/*
+ID: icystuf1
+PROG: gift1
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+using namespace std;
+
+
+
+int main()
+{
+    ifstream fin("gift1.in");
+    ofstream fout("gift1.out");
+
+    
+}
