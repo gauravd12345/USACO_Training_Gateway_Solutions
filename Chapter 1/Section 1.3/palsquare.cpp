b62ef49b115a6bd774e108e9e82831981f8c3e9d
gauravd12345 <55636921+gauravd12345@users.noreply.github.com>
2020-11-11 11:14:34 -0800

    Update palsquare.cpp

--- Chapter 1/Section 1.3/palsquare.cpp
+++ Chapter 1/Section 1.3/palsquare.cpp
@@ -1,1 +1,22 @@
-a
+/*
+ID: icystuf1
+PROG: palsquare
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+using namespace std;
+
+
+
+int main()
+{
+    ifstream fin("palsquare.in");
+    ofstream fout("palsquare.out");
+
+    
+}
