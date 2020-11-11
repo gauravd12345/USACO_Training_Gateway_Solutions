aa4708d079b728587fc0f502925285c267b9ba54
gauravd12345 <55636921+gauravd12345@users.noreply.github.com>
2020-11-11 11:12:11 -0800

    Update ride.cpp

--- Chapter 1/Section 1.2/ride.cpp
+++ Chapter 1/Section 1.2/ride.cpp
@@ -1,1 +1,22 @@
-a
+/*
+ID: icystuf1
+PROG: ride
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+using namespace std;
+
+
+
+int main()
+{
+    ifstream fin("ride.in");
+    ofstream fout("ride.out");
+
+    
+}
