11923bcf8efeb3e5398ac81aba66ce04752f7316
gauravd12345 <55636921+gauravd12345@users.noreply.github.com>
2020-11-11 11:13:42 -0800

    Update milk2.cpp

--- Chapter 1/Section 1.3/milk2.cpp
+++ Chapter 1/Section 1.3/milk2.cpp
@@ -1,1 +1,22 @@
-a
+/*
+ID: icystuf1
+PROG: milk2
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+using namespace std;
+
+
+
+int main()
+{
+    ifstream fin("milk2.in");
+    ofstream fout("milk2.out");
+
+    
+}
