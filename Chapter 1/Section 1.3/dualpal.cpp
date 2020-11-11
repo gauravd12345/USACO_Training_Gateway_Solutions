e02e2da6df423b897d9f0695d054aa9d2df92f36
gauravd12345 <55636921+gauravd12345@users.noreply.github.com>
2020-11-11 11:13:07 -0800

    Update dualpal.cpp

--- Chapter 1/Section 1.3/dualpal.cpp
+++ Chapter 1/Section 1.3/dualpal.cpp
@@ -1,1 +1,22 @@
-a
+/*
+ID: icystuf1
+PROG: dualpal
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+using namespace std;
+
+
+
+int main()
+{
+    ifstream fin("dualpal.in");
+    ofstream fout("dualpal.out");
+
+    
+}
